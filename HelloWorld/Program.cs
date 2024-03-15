2ec1d42dc80de7197ad08b78c6a94e72f617d660
Denis Batista de Andrade <34269902+deniszola@users.noreply.github.com>
2024-03-15 14:19:00 -0300

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!\n");
+
+            string text = "3 + 3 = ";
+            int num = 3 + 3;
+            bool myBool = true;
+            if (myBool)
+            {
+                Console.WriteLine(text + num);
+            }
+            else
+            {
+                Console.WriteLine("myBool is false");
+            }
+        }
+    }
+}
